98797816858f5339bd0656e1a51f2558f626d7fb
AbhishekChoudhari42 <choudhariabhishek93@gmail.com>
2024-03-31 13:24:59 +0530

    style changes

--- app/components/NotePad.tsx
+++ app/components/NotePad.tsx
@@ -36,23 +36,27 @@ const addText = () =>{
     // },[])
 
 return (
-    <div className='w-full h-[100dvh] bg-black p-4 pt-[50px]'>
-        {
+    <div className='relative flex flex-col w-full h-[100dvh]  p-4  max-w-[400px] overflow-y-scroll'>
+        <div className='overflow-y-scroll flex-grow'>
+        {   
+
             notes.map((el,index)=>{
-                return <div key={index} className='w-full min-h-fit'>
-                           <div className='flex justify-between w-full'>
+                return <div key={index} className='w-full min-h-fit mb-4 pr-2'>
+                           <div className='flex justify-between w-full mb-4'>
                                 <p className='text-sm text-neutral-500'>{el.date}</p>
                                 <p className='text-sm text-neutral-500 hover:text-neutral-100'>Edit</p>
                             </div> 
                             <p className='text-md'>{el.content}</p>
+                            <hr className='mt-4 border-t-neutral-800'/>
                         </div>
             })
         }
-        <div className='w-full max-w-[400px] fixed bottom-4'>
+            </div>
+        <div className='w-full max-w-[400px] bg-black'>
             <textarea onChange={(e)=>{setText(e.target.value)}} value={text} className='w-full mb-4 bg-neutral-800 p-2' ></textarea>
             <button onClick={addText} className='w-full p-4 bg-indigo-600'>Add</button>
-            <button className='fixed top-0 right-0 w-[100px] p-2 bg-green-500'>Save</button>
         </div>
+        {/* <button className='absolute top-0 right-0 w-[100px] p-2 bg-green-500'>Save</button> */}
     </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import NotePad from "./components/NotePad";
 
 export default function Home() {
   return (
-    <main className="w-screen min-h-screen">
+    <main className="w-screen min-h-screen flex justify-center items-center">
       <NotePad/>
     </main>
   );
